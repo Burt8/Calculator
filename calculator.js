00e2888934b8a3fad9de870b67b00c294112fe6a
Burt <154017141+Burt8@users.noreply.github.com>
2024-06-03 16:50:34 -0700

    declare button elements

---
+++ calculator.js
@@ -0,0 +1,17 @@
+//number buttons
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let four = document.querySelector('.four');
+let five = document.querySelector('.five');
+let six = document.querySelector('.six');
+let seven = document.querySelector('.seven');
+let eight = document.querySelector('.eight');
+let nine = document.querySelector('.nine');
+let zero = document.querySelector('.zero');
+//function buttons
+let add = document.querySelector('.add')
+let subtract = document.querySelector('.subtract')
+let multiply = document.querySelector('.multiply')
+let divide = document.querySelector('.divide')
+let clear = document.querySelector('.clear')
